1aa24068b353bc723ad0ee7b4e4cb2aa5ed80dc2
TC-ghub <thoms.constantin@gmail.com>
2025-12-03 15:43:59 +0100

    Test pathing3

--- films_final.py
+++ films_final.py
@@ -9,5 +9,5 @@ print(f"Script location: {Path(__file__).resolve()}")
 print(f"CSV file path: {resolved_path}")
 print(f"CSV file exists: {csv_path.exists()}\n")
 
-films = pd.read_csv(csv_path)
+films = pd.read_csv(resolved_path)
 print(films.head(5))
